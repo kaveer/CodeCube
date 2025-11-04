61f0de99e91267f1e8fd3dc25bbd6d663f057311
kaveer <kaveer.rajcoomar@sdworx.com>
2025-11-04 09:51:51 +0400

    add bin and obj folder

--- BmiCalculator/obj/Debug/net9.0-android/BmiCalculator.AssemblyInfo.cs
+++ BmiCalculator/obj/Debug/net9.0-android/BmiCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BmiCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3d589e8432d617ddaa183f92ad63d865af246160")]
 [assembly: System.Reflection.AssemblyProductAttribute("BmiCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BmiCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
